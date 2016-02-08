9840ce783ac00bc085e3a4861c4c095c7bf0acf7
mFelgate <m.felgate55@gmail.com>
2016-02-07 21:57:43 -0500

    edits

--- toBeTested.c
+++ toBeTested.c
@@ -20,40 +20,46 @@ int numOfGenrations = cin.get();
 
 //more of a idea
 int board = arr[totalRows][totalCols];
-CreateBoard(totalCols,totalRows);
 
 cout << ESC << "[H" << ESC << "[J" << "Initial:" << endl;
 while(numOfGenrations>0){
   cout << ESC << "[H" << "Generation " << numOfGenrations << ":" << endl;
-  generation(totalCols,totalRows);
+
+  for(int x = 0; x<= rows; x++){
+    for(int y = 0; y<= cols; y++){
+      if (x == 0 || x == cols || y == 0 || y == rows){
+        if (x == 0  && y ==0)
+          cout<<"+";
+        else if (x == cols  && y == rows)
+          cout<<"+";
+        else if (x == 0  || x == cols)
+          cout<<"|";
+        else if ( y == 0  || y == cols)
+          cout<<"-"
+           }
+    else{
+        if(countOrganisms(x,y)<2 && Organism _board[x][y] == LIVING){
+            Organism _board[x][y] = DYING;
+            cout<<"*";}
+        else if(countOrganisms(x,y)>3 && Organism _board[x][y] == LIVING){
+          Organism _board[x][y] = DYING;
+          cout<<"*";}
+        else if(Organism _board[x][y] == DYING){
+          Organism _board[x][y] = NONE;
+          cout<<" ";}
+       else if(Organism _board[x][y] == GESTATING){
+         Organism _board[x][y] = LIVING;
+         cout<<"*";}
+        else if(countOrganisms(x,y) && Organism _board[x][y] == LIVING){
+        Organism _board[x][y] = LIVING;
+        cout<<"*";}
+        else
+          cout<<" ";}
   numOfGenrations--;
 }
-
+}}
 }
 
-int generation(cols, rows){
-for(int x = 0; x<= rows; x++){
-  for(int y = 0; y<= cols; y++){
-
-    if(Organism _board[x][y] == DYING){
-        Organism _board[x][y] = NONE;
-        coutt<<"*";}
-
-    if(Organism _board[x][y] == living){
-      if(countOrganisms(x,y)<2)
-        Organism _board[x][y] = DYING;
-      else if(countOrganisms(x,y)>3)
-        Organism _board[x][y] = DYING;
-      coutt<<"*";}
-
-    if(Organism _board[x][y] == GESTATING){
-      Organism _board[x][y] = LIVING;
-      cout<<" ";}
-
-    }
-
-    }
-  }
 
 int countOrganisms ( int x, int y){
   int count = 0;
@@ -74,21 +80,4 @@ int countOrganisms ( int x, int y){
   if(Organism[x+1][y-1] == Living || Organism[x][y] == DYING)
     count++;
   return (count);
-}
-
-int createBoard(cols,rows){
-  for (int x = 0; x<= cols; x++){
-    for(int y = 0; y<= rows; y++){
-
-       if (x == 0 || x == cols || y == 0 || y == rows){
-         if (x == 0  && y ==0)
-           cout<<"+";
-          else if (x == cols  && y == rows)
-            cout<<"+";
-          else if (x == 0  || x == cols)
-            cout<<"|";
-         else
-            cout<<"-"
-       }
-}}
 }

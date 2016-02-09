df121fb7cb89b239b5631178861ed383f5ff4e30
Samuel Colacchia <samuel.colacchia@gordon.edu>
2016-02-09 16:42:24 -0500

    completed user input

--- working.c
+++ working.c
@@ -2,28 +2,35 @@
 using namespace std;
 int main()
 {
-        int numOFOrganisms, numOfGenrations, initLocations;
+        int numOFOrganisms, numOfGenrations;
+        char initLocations[100];
+        enum Organism { NONE, GESTATING, LIVING, DYING, BORDER };
 
         //Get the starting orgonisms then clear the input buffer just to be safe
         cout<<"Number of starting organisms: ";
         cin >> numOFOrganisms;
         while (cin.get() != '\n');
 
+        //Get the starting locations store them and asign them to a var,
+        //TODO may consider changing and improving the way of input
         cout<<"Locations: ";
-        cin >> initLocations;
-
+        cin.getline(initLocations, sizeof(initLocations));
+        while (cin.get() != '\n');
 
         //Get the number of generations then clear the input buffer just to be safe
         cout << "Number of generations: ";
         cin >> numOfGenrations;
         while (cin.get() != '\n') ;
 
+        for (int i = 0; i < numOfGenrations; i++) {
+          
+        }
 
         //         char name2[50];
         // cin >> name2;
         // cout << name;
         // cout << name2;
-        enum Organism { NONE, GESTATING, LIVING, DYING, BORDER };
+
       //  Organism _board[10][10] = {NONE};
       //  int j = 0;
         // static const char ESC = 27;

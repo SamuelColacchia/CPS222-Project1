b21ff570227d4825c92b5ca28eb0efdcedf902b1
Samuel Colacchia <samuel.colacchia@gordon.edu>
2016-02-09 23:59:48 -0500

    A little cleanup

--- working.cc
+++ working.cc
@@ -27,39 +27,6 @@ int countOrganisms(Organism _board[totalRows][totalCols], int rows, int cols)
          }
       }
    }
-
-   //  if ((_board[rows + 1][cols] == LIVING) || (_board[rows + 1][cols] == DYING))
-   //  {
-   //     count++;
-   //  }
-   //  if ((_board[rows][cols + 1] == LIVING) || (_board[rows][cols + 1] == DYING))
-   //  {
-   //     count++;
-   //  }
-   //  if ((_board[rows + 1][cols + 1] == LIVING) || (_board[rows + 1][cols + 1] == DYING))
-   //  {
-   //     count++;
-   //  }
-   //  if ((_board[rows - 1][cols] == LIVING) || (_board[rows - 1][cols] == DYING))
-   //  {
-   //     count++;
-   //  }
-   //  if ((_board[rows][cols - 1] == LIVING) || (_board[rows][cols - 1] == DYING))
-   //  {
-   //     count++;
-   //  }
-   //  if ((_board[rows - 1][cols - 1] == LIVING) || (_board[rows - 1][cols - 1] == DYING))
-   //  {
-   //     count++;
-   //  }
-   //  if ((_board[rows - 1][cols + 1] == LIVING) || (_board[rows - 1][cols + 1] == DYING))
-   //  {
-   //     count++;
-   //  }
-   //  if ((_board[rows + 1][cols - 1] == LIVING) || (_board[rows + 1][cols - 1] == DYING))
-   //  {
-   //     count++;
-   //  }
    return(count);
 }
 
@@ -68,7 +35,7 @@ int main()
 {
    int numOFOrganisms, numOfGenrations;
 
-   // Create Board
+   // Create Boards
    Organism _board[totalRows][totalCols] =
    {
       NONE
@@ -78,8 +45,6 @@ int main()
       NONE
    };
 
-   // TODO Known Bugs
-   // 1: Does not handle user input thing well.
 
    // Get the starting orgonisms then clear the input buffer just to be safe
    cout << "Number of starting organisms: ";
@@ -89,8 +54,7 @@ int main()
    }
 
 
-   // Get the starting locations store them and asign them to a var,
-
+   // Get and set starting locations
    for (int i = 0; i < numOFOrganisms; i++)
    {
       int a;
@@ -104,8 +68,6 @@ int main()
       }
    }
 
-
-   // while (cin.get() != '\n');
 
    //Get the number of generations then clear the input buffer just to be safe
    cout << "Number of generations: ";
@@ -128,7 +90,6 @@ int main()
       }
    }
 
-   //Copy
 
 
    //The main loop
@@ -188,7 +149,7 @@ int main()
             {
                if (countOrganisms(_oldboard, x, y) == 3)
                {
-                   
+
                   _board[x][y] = GESTATING;
                }
             }
@@ -215,51 +176,4 @@ int main()
       {
       }
    }
-// Display board as is ##TESTING PUPOSE##
-
-
-// for (int i = 0; i < numOfGenrations; i++) {
-//
-// }
-
-// char name2[50];
-// cin >> name2;
-// cout << name;
-// cout << name2;
-
-// Organism _board[10][10] = {NONE};
-// int j = 0;
-// static const char ESC = 27;
-// cout << ESC << "[H" << ESC << "[J" << "Initial:" << endl;
-
-// old method to build a board
-// will keep around, may be useful in the future
-// for (int i=1; i <= (sizeof(_board)/sizeof(int)) ; i++) {
-// if (i % 10 == 1) {
-// cout << "start";
-// }
-// cout << i;
-// if (i % 10 == 0 && i != 0) {
-// cout << "end of row"<< j++ << endl;
-//
-// }
-// }
-
-// Better way to get board information.
-// for (int x = 0; x <= 10; x++){
-// for (int y = 0; y < 10; y++){
-// Organism _board[x][y]=LIVING;
-// }
-// }
-// for (int x = 0; x < 10; x++) {
-// cout << "|" << "-";
-// for (int y = 0; y < 10; y++) {
-// _board[x][y] = LIVING;
-// if (_board[x][y] == LIVING) {
-// cout << "#" << "-";
-// }
-// }
-// cout << "|" << endl;
-// }
-// cout << (sizeof(_board)/sizeof(int)) << endl;
 }
